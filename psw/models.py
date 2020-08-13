cef077e5f4e274d1ec4dffbdf856b50a9ba7b7f9
Modou Diao <modou.diao@wizall.com>
2020-08-13 15:28:56 +0000

    update the model and add utils folder

--- psw/models.py
+++ psw/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 # Create your models here.
 
+NIVEAU =(
+    (1, "Sixieme College"),
+    (2, "Cinquieme College"),
+    (4, "Sixieme College"),
+    (5, "troisieme College"),
+    (6, "Seconde Lycee"),
+    (7, "Premiere Lycee"),
+    (0, "Terminale Lycee"),
+)
 class BaseEntity(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -11,7 +20,8 @@ class BaseEntity(models.Model):
 
 class Exercice(BaseEntity):
     name = models.CharField(max_length = 100)
-    exo_file = models.FileField(upload_to='media',blank=False)
+    exo_file = models.FileField(upload_to='media',blank=True, null=True)
+    niveau  = models.CharField(max_length = 6, choices = NIVEAU, default=0)
 
     def __str__(self):
-    	return self.exo_file
+    	return self.name
